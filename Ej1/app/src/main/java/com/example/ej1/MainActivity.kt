bfa1cb88695a2c329dc452675b7f06a7e0e80586
PaulafloresS <61991692+PaulafloresS@users.noreply.github.com>
2023-04-19 21:16:01 -0600

    Primeros 3 campos

--- Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
+++ Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.ej1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.ej1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    fun click(view: View) {
+
+       if(binding.etNombre.text.isEmpty()){
+            binding.etNombre.error= R.string.valor_requerido.toString()
+           Toast.makeText(this, getString(R.string.ingresa_valor), Toast.LENGTH_LONG).show()
+        }
+
+
+        if(binding.etApellido.text.isEmpty()){
+            binding.etApellido.error= R.string.valor_requerido.toString()
+            Toast.makeText(this, getString(R.string.ingresa_valor), Toast.LENGTH_LONG).show()
+        }
+
+        if(binding.etEmail.text.isEmpty()){
+            binding.etEmail.error= R.string.valor_requerido.toString()
+            Toast.makeText(this, getString(R.string.ingresa_valor), Toast.LENGTH_LONG).show()
+        }
+
     }
 }
