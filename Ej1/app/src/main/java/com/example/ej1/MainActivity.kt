3034363b48fb240652e96773b733960a4c3536b1
PaulafloresS <61991692+PaulafloresS@users.noreply.github.com>
2023-04-29 23:21:16 -0600

    horoscopos

--- Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
+++ Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
@@ -31,7 +31,80 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
+        val tv = findViewById<TextView>(R.id.tv_fechaNac)
+        val cal = Calendar.getInstance()
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+      //  binding.tvFechaNac.text = calculateAge(binding.).toString()
+        //spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val lista = resources.getStringArray(R.array.carreras)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
+        spinner.adapter = adaptador
+        val btn: Button = findViewById(R.id.boton)
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        //date picker
+
+
+        tv.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                    val selectedDate = Calendar.getInstance()
+                    selectedDate.set(selectedYear, selectedMonth, selectedDayOfMonth)
+
+                    tv.text = "Date: $selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+
+                     age = calculateAge(selectedDate.timeInMillis)
+                    // Aquí puedes usar la variable age para hacer lo que necesites con la edad calculada
+                },
+                year,
+                month,
+                day
+            )
+
+            datePickerDialog.datePicker.maxDate =
+                System.currentTimeMillis() // Aquí se establece la fecha máxima permitida
+
+            datePickerDialog.show()
+        }
+
+        //calculando horoscopos
+
+
+
+    }
+
+    //calculando la edad
+    private fun calculateAge(birthdate: Long): Int {
+        val dob = Calendar.getInstance()
+        dob.timeInMillis = birthdate
+
+        val today = Calendar.getInstance()
+        var age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
+
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
+            age--
+        }
+
+        return age
 
     }
 
