a247ea5111cbb33d7956b5ca221c6575c892559a
PaulafloresS <61991692+PaulafloresS@users.noreply.github.com>
2023-04-29 23:19:57 -0600

    horoscopos

--- Ej1/app/src/main/java/com/example/ej1/Datos.kt
+++ Ej1/app/src/main/java/com/example/ej1/Datos.kt
@@ -22,9 +22,67 @@ class Datos : AppCompatActivity() {
 
         binding.idtvCorreo.text = email
         binding.idtvNumCuenta.text = numCuenta
-       binding.idtvEdad.text = age
+        binding.idtvEdad.text = age
         binding.idtvApellido.text = nombre
         binding.idtvNombre.text = apellido
+
+
+
+        //calculando el zodiaco
+        fun zodiaco(day: Int, month: Int) {
+            when {
+                (month == 1 && day >= 20 || month == 2 && day <= 19) -> {
+                    binding.tvZodiacal.text ="@string/acuario"
+                    binding.ivZodiaco.setImageResource(R.drawable.acuario)
+                }
+                (month == 2 && day >= 20 || month == 3 && day <= 20) -> {
+                    binding.tvZodiacal.text ="@string/picis"
+                    binding.ivZodiaco.setImageResource(R.drawable.piscis)
+                }
+                (month == 12 && day >= 22 || month == 1 && day <= 19) -> {
+                    binding.tvZodiacal.text ="@string/capricornio"
+                    binding.ivZodiaco.setImageResource(R.drawable.capricornio)
+                }
+                (month == 11 && day >= 23 || month == 12 && day <= 21) -> {
+                    binding.tvZodiacal.text ="@string/sagitario"
+                    binding.ivZodiaco.setImageResource(R.drawable.sagitario)
+                }
+                (month == 10 && day >= 23 || month == 11 && day <= 22) -> {
+                    binding.tvZodiacal.text ="@string/escorpion"
+                    binding.ivZodiaco.setImageResource(R.drawable.escorpio)
+                }
+                (month == 9 && day >= 23 || month == 10 && day <= 22) -> {
+                    binding.tvZodiacal.text ="@string/libra"
+                    binding.ivZodiaco.setImageResource(R.drawable.libra)
+                }
+                (month == 8 && day >= 23 || month == 9 && day <= 22) -> {
+                    binding.tvZodiacal.text ="@string/virgo"
+                    binding.ivZodiaco.setImageResource(R.drawable.virgo)
+                }
+                (month == 7 && day >= 23 || month == 8 && day <= 22) -> {
+                    binding.tvZodiacal.text ="@string/leo"
+                    binding.ivZodiaco.setImageResource(R.drawable.leo)
+                }
+                (month == 6 && day >= 21 || month == 7 && day <= 22) -> {
+                    binding.tvZodiacal.text ="@string/cancer"
+                    binding.ivZodiaco.setImageResource(R.drawable.cancer)
+                }
+                (month == 5 && day >= 21 || month == 6 && day <= 20) -> {
+                    binding.tvZodiacal.text ="@string/geminis"
+                    binding.ivZodiaco.setImageResource(R.drawable.geminis)
+                }
+                (month == 4 && day >= 21 || month == 5 && day <= 20) -> {
+                    binding.tvZodiacal.text ="@string/tauro"
+                    binding.ivZodiaco.setImageResource(R.drawable.tauro)
+                }
+                (month == 3 && day >= 21 || month == 4 && day <= 20) -> {
+                    binding.tvZodiacal.text ="@string/aries"
+                    binding.ivZodiaco.setImageResource(R.drawable.aries)
+                }
+                else -> "Input invalido"
+            }
+        }
+
 
     }
 
--- Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
+++ Ej1/app/src/main/java/com/example/ej1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewParent
 import android.widget.AdapterView
@@ -22,83 +23,15 @@ import java.util.regex.Pattern
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    //var patron:Pattern=Pattern.compile("^[^@]+@[^@]+\\.[a-zA-Z]{2,}\$")
-    //var comparador:Matcher = patron.matcher(binding.etEmail.text)
+
 
     var bandera = true
     var age =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-      //  binding.tvFechaNac.text = calculateAge(binding.).toString()
-        //spinner
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        val lista = resources.getStringArray(R.array.carreras)
-        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
-        spinner.adapter = adaptador
-        val btn: Button = findViewById(R.id.boton)
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long) {
-            }
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-
-        }
-        //date picker
-        val tv = findViewById<TextView>(R.id.tv_fechaNac)
-        val cal = Calendar.getInstance()
-        val year = cal.get(Calendar.YEAR)
-        val month = cal.get(Calendar.MONTH)
-        val day = cal.get(Calendar.DAY_OF_MONTH)
-
-        tv.setOnClickListener {
-            val datePickerDialog = DatePickerDialog(
-                this,
-                { _, selectedYear, selectedMonth, selectedDayOfMonth ->
-                    val selectedDate = Calendar.getInstance()
-                    selectedDate.set(selectedYear, selectedMonth, selectedDayOfMonth)
-
-                    tv.text = "Date: $selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
-
-                     age = calculateAge(selectedDate.timeInMillis)
-                    // Aquí puedes usar la variable age para hacer lo que necesites con la edad calculada
-                },
-                year,
-                month,
-                day
-            )
-
-            datePickerDialog.datePicker.maxDate =
-                System.currentTimeMillis() // Aquí se establece la fecha máxima permitida
-
-            datePickerDialog.show()
-        }
-
-        //datos
 
 
-    }
-
-    //calculando la edad
-    private fun calculateAge(birthdate: Long): Int {
-        val dob = Calendar.getInstance()
-        dob.timeInMillis = birthdate
-
-        val today = Calendar.getInstance()
-        var age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
-
-        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
-            age--
-        }
-
-        return age
 
     }
 
@@ -182,6 +115,7 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("email", binding.etEmail.text.toString())
             intent.putExtra("num_cuenta", binding.etNumCuenta.text.toString())
             intent.putExtra("fecha_nac", age.toString())
+
             startActivity(intent)
 
         }
